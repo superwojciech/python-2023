90f24d4024d0c5f816624f1196fd8ee17891740d
Algorytmix1337 <kontodogithuba@o2.pl>
2023-05-05 23:55:25 +0200

    Fixed zad B

--- src/algo/z6/b.py
+++ src/algo/z6/b.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def has_duplicates(lista):
     duplicates = []
     no_duplicates = [] 
@@ -14,20 +13,9 @@ def has_duplicates(lista):
     else:
         print("Nie ma powtórzeń")
         return
-    #return len(lista) != len(set(no_duplicates))
+   
 
 
 
 a=['aa', 'a', 'c', 'bb']
 has_duplicates(a)
-=======
-def has_duplicates_string(lista):
-    print(len(lista))
-    print(len(set(lista)))
-    return len(lista) != len(set(lista))
-
-
-
-a=['xxx', 'www', 'yyy', 'abc']
-print(has_duplicates_string(a))
->>>>>>> c5c9a331f1e91f39545037b3fe9f187f433d93f0
---
+++ src/algo/z6/c.py
@@ -0,0 +1,8 @@
+a_list = [(5,'Adam'), (3,'Jane'), (5, 'Xiao'), (2,'Jane')]
+b = []
+for x in a_list:
+    if x not in b:
+        b.append(x)
+
+print (b)
+    
