0c7e89123d9f1d75eb7bf4ebe133483c5cc7d4b3
unknown <wkorczynski22@gmail.com>
2023-05-07 11:51:50 +0200

    dodano zadanie C & D - 15.04

--- src/algo/z6/c.py
+++ src/algo/z6/c.py
@@ -1,11 +1,13 @@
-a_list = [(5,'Adam'), (3,'Jane'), (5, 'Xiao'), (2,'Jane')]
-b = []
-for x in a_list:
-    if x not in b:
-        b.append(x)
+data = [(5,'Adam'), (3,'Jane'), (5, 'Xiao'), (2,'Jane')]
 
-print (b)
-    
+d = {}
 
-    #test
-    #test
+for id, name in data:
+    if id not in d:
+        d[id] = [name]
+    else:
+        d[id].append(name)
+
+for id, names in d.items():
+    if len(names) > 1:
+        print(f"Do ID: {id} przypisane są imiona takie jak: {names}")
