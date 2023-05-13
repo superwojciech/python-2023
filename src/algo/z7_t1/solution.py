1ff22fdd0f9f82b22857eb419a0730dc1bcb69b0
Wojciech Korczyński <wkorczynki22@gmail.com>
2023-05-13 14:46:57 +0200

    add comments

--- src/algo/z7_t1/solution.py
+++ src/algo/z7_t1/solution.py
@@ -16,10 +16,11 @@ jeśli długości *'ek klucza + długości #'ów zamka są dla wszystkich takie 
 
 
 def open_lock(key: list[int], lock: list[int]) -> int:
-    
+    # sprawdzenie czy wartości nie są < 0
     if any(x < 0 for x in key) or any(x < 0 for x in lock):
         return -1    
     
+    #proba otworzenia zamka
     key_index = 0
     lock_index = 0
     while key_index < len(key) and lock_index < len(lock):
@@ -30,6 +31,6 @@ def open_lock(key: list[int], lock: list[int]) -> int:
             key_index += 1
     
     if lock_index == len(lock):
-        return 0  
+        return 0  #zamek sie otwiera
     else:
-        return 1  
+        return 1  #błędny klucz
