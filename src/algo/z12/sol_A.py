0965fc355c0bb20a30d0496c35104737586a1839
unknown <wkorczynski22@gmail.com>
2023-07-01 11:50:29 +0200

    sol A

--- src/algo/z12/sol_A.py
+++ src/algo/z12/sol_A.py
@@ -1,3 +1,12 @@
 def get_min_number_of_operations(a: list[int]) -> int:
-    # todo: your code here
-    return 0
+    x = 0
+    y = 0
+    length = len(a)
+    while length > y:
+        if a[y] < 0:
+            x = x +1
+            while a[y] <= 0:
+                   y = y+1
+                   if length == y: break
+        y = y +1
+    return x
